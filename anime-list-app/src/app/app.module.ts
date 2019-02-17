0ba8bb2c5bb59219b8ed2b29207326b096649e12
Catherine Im <ci6nq@virginia.edu>
2019-02-17 05:28:29 -0500

    material select form with native select, started results section

--- anime-list-app/src/app/app.module.ts
+++ anime-list-app/src/app/app.module.ts
@@ -9,8 +9,9 @@ import { InputParametersService } from './input-parameters.service';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSlideToggleModule, MatSliderModule, MatInputModule, MatButtonModule, } from '@angular/material';
+import {MatToolbarModule, MatDividerModule, MatSlideToggleModule, MatSliderModule, MatInputModule, MatButtonModule, MatSelectModule} from '@angular/material';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 
 
@@ -18,13 +19,17 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    SearchResultsComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
+    MatDividerModule,
     MatInputModule,
     MatButtonModule,
     MatSliderModule,
+    MatSelectModule,
     MatSlideToggleModule,
     FormsModule,
     MatAutocompleteModule,
