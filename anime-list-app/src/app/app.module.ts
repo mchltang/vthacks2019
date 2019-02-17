4a67d8d5505471fc26cf82e881e80ca55af7bc71
Catherine Im <ci6nq@virginia.edu>
2019-02-17 08:09:25 -0500

    everything works

--- anime-list-app/src/app/app.module.ts
+++ anime-list-app/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { InputParametersService } from './input-parameters.service';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatToolbarModule, MatDividerModule, MatSlideToggleModule, MatSliderModule, MatInputModule, MatButtonModule, MatSelectModule} from '@angular/material';
+import { MatListModule, MatCardModule, MatToolbarModule, MatDividerModule, MatSlideToggleModule, MatSliderModule, MatInputModule, MatButtonModule, MatSelectModule} from '@angular/material';
 import { MatAutocompleteModule} from '@angular/material/autocomplete';
 import { SearchResultsComponent } from './search-results/search-results.component';
 
@@ -23,6 +23,7 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     SearchResultsComponent
   ],
   imports: [
+    MatListModule,
     MatCardModule,
     MatToolbarModule,
     BrowserModule,
--- anime-list-app/src/app/search-results/search-results.component.ts
+++ anime-list-app/src/app/search-results/search-results.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent implements OnInit {
-  recList: string[] = ['Alabama', 'Virginia', 'Washington'];
+  recList: string[] = ['Alabama', 'Virginia', 'Washington', 'A','B', 'c', 'd', 'e', 'f', 'g'];
 
   constructor() { }
 
